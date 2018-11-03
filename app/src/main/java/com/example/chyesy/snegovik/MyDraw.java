cc216afe187a4f6430ba97e4201da58f2bd90b3f
Viviling <44644025+Viviling@users.noreply.github.com>
2018-11-04 00:21:52 +0700

    исправил положение снеговика и добавил деталей

--- app/src/main/java/com/example/chyesy/snegovik/MyDraw.java
+++ app/src/main/java/com/example/chyesy/snegovik/MyDraw.java
@@ -24,22 +24,29 @@ public class MyDraw extends View {
         paint.setColor(Color.WHITE);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setStrokeWidth(1);
-        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight() * 3 / 4 + 40, 100, paint);
-        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight() * 3 / 4 - 115, 75, paint);
-        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight() * 3 / 4 - 230, 50, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight()-110, 100, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight()-265, 75, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4, getHeight()-370, 50, paint);
         canvas.drawRect(0, this.getHeight() - 10, this.getWidth(), this.getHeight(), paint);
         paint.setColor(0x85FF0012);
-        canvas.drawCircle(this.getWidth() * 3 / 4 - 20, getHeight() * 3 / 4 - 240, 7, paint);
-        canvas.drawCircle(this.getWidth() * 3 / 4 + 20, getHeight() * 3 / 4 - 240, 7, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4 - 20, getHeight()-370, 7, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4 + 20, getHeight()-370, 7, paint);
         paint.setColor(Color.BLACK);
-        canvas.drawCircle(this.getWidth() * 3 / 4 - 20, getHeight() * 3 / 4 - 240, 2, paint);
-        canvas.drawCircle(this.getWidth() * 3 / 4 + 20, getHeight() * 3 / 4 - 240, 2, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4 - 20, getHeight()-370, 2, paint);
+        canvas.drawCircle(this.getWidth() * 3 / 4 + 20, getHeight()-370, 2, paint);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
-
         final RectF oval = new RectF();
-        oval.set(this.getWidth() * 3 / 4 - 30, getHeight() * 3 / 4 - 180 - 30, this.getWidth() * 3 / 4 + 30, getHeight() * 3 / 4 - 180 + 30);
+        oval.set(this.getWidth() * 3 / 4 - 30, getHeight()-379, this.getWidth() * 3 / 4 + 30, getHeight()-359);
         canvas.drawArc(oval, 230, 80, false, paint);
+        oval.set(this.getWidth() * 3 / 4 - 30, getHeight()-345, this.getWidth() * 3 / 4 + 30, getHeight()-325);
+        canvas.drawArc(oval, 230, 80, false, paint);
+        //
+        paint.setColor(Color.RED);
+        canvas.drawRect(this.getWidth() * 3 / 4 -40, getHeight()-450, this.getWidth() * 3 / 4 +40, this.getHeight()-400, paint);
+       //
         //ножка от елки
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(this.getWidth() ,0, 150, paint);
         paint.setColor(0xFF762D00);
         canvas.drawRect(230, this.getHeight() - 55, 270, this.getHeight() - 10, paint);
         //елка
